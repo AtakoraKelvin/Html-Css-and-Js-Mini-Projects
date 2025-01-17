680f3e88329f7c4be05e1eb6c828c2702bbe6243
Tough 1K <atakorakelvin257@gmail.com>
2025-01-17 17:15:50 +0000

    add final touch's to dark mode toggler

--- Dark Mode BG Toggler/main.js
+++ Dark Mode BG Toggler/main.js
@@ -1,11 +1,9 @@
 const toggler = document.querySelector("#toggler");
 
 toggler.addEventListener("click", (e) => {
-    if (e.checked) {
-        document.body.classList.remove("light");
-        document.body.classList.add("dark");
+    if (e.target.checked) {
+        document.body.style.background = "#000";
     } else {
-        document.body.classList.remove("dark");
-        document.body.classList.add("light");
+        document.body.style.background = "#fff";
     }
 });
